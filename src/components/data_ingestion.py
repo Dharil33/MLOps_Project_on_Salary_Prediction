6d8689955d870b8c21cc409e80d87200298ddd6b
Dharil33 <dharilpatel61857@gmail.com>
2023-08-16 23:04:24 +0530

    model training is completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from boto3.session import Session
 from decouple import config
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 ACCESS_KEY_ID = config('ACCESS_KEY_ID')
 SECRET_KEY = config('SECRET_KEY')
@@ -54,4 +55,5 @@ if __name__ == "__main__":
     train_data,test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
-    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
